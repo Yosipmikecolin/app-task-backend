832f5c82b5e5ae698bf237a275a436a19c3cbaf6
Yosip Mike Colin <colinparrado@gmail.com>
2025-12-10 16:52:00 -0500

    Creation of the Mongoose model

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,21 @@
-export class Task {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type TaskDocument = HydratedDocument<Task>;
+
+@Schema()
+export class Task {
+  @Prop()
+  title: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  completed: boolean;
+
+  @Prop({ default: Date.now })
+  createdAt: Date;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
