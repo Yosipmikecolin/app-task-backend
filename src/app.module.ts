e8ffa1e5f17942005379f8249b7f98beebbed751
Yosip Mike Colin <colinparrado@gmail.com>
2025-12-10 17:36:14 -0500

    Configuring environment variables and connecting to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TasksModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.getOrThrow<string>('MONGO_URL'),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
