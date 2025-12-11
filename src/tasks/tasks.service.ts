1e7a9997beb37dd5af8f931d8837cf8cdf69523e
Yosip Mike Colin <colinparrado@gmail.com>
2025-12-10 19:49:32 -0500

    Creation of the service to create tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './entities/task.entity';
+import { handleMongoError } from 'src/utils/handle-mongo-error.util';
 
 @Injectable()
 export class TasksService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
+
+  async create(createTaskDto: CreateTaskDto) {
+    try {
+      const createdTask = new this.taskModel(createTaskDto);
+      return await createdTask.save();
+    } catch (error) {
+      throw handleMongoError(error);
+    }
   }
 
   findAll() {
